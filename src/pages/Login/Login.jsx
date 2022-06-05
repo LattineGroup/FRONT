4577f81b604b173f417ca3261a72dddfc2ef2630
Bielmpg <99812402+Bielmpg@users.noreply.github.com>
2022-06-05 13:00:58 -0300

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -24,7 +24,7 @@ export default class Login extends Component {
         event.preventDefault();
 
         this.setState({ erroMensagem: '', isLoading: true });
-        axios.post('https://door-back-lattine.azurewebsites.net/api/Login/Login'), {
+        axios.post('https://door-back.azurewebsites.net/index.html'), {
             email: this.state.email,
             senha: this.state.senha
         })
