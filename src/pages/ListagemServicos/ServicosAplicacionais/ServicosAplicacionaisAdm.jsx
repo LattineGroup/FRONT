4f1cdaa4359e2b878c4c5282a01b153ceb9f318c
Bielmpg <99812402+Bielmpg@users.noreply.github.com>
2022-06-05 13:43:58 -0300

    Update ServicosAplicacionaisAdm.jsx

--- src/pages/ListagemServicos/ServicosAplicacionais/ServicosAplicacionaisAdm.jsx
+++ src/pages/ListagemServicos/ServicosAplicacionais/ServicosAplicacionaisAdm.jsx
@@ -9,7 +9,7 @@ export default function ServicosAplicacionais() {
     const [dataCadastro, setDataCadastro] = useState("");
 
     function buscarServicos() {
-        axios('http://localhost:5000/api/ServicoAplicacionals', {
+        axios('https://door-back.azurewebsites.net/api/ServicoAplicacionals', {
             headers: {
                 'Authorization': 'Bearer ' + localStorage.getItem('usuario-login')
             }
