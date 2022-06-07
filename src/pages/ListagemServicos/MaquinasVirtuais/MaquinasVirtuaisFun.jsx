80d16249c66a7647ac16ff7b3ea4b90a7f62b8f9
LattineGroup <106936246+LattineGroup@users.noreply.github.com>
2022-06-07 20:29:03 -0300

    Update MaquinasVirtuaisFun.jsx

--- src/pages/ListagemServicos/MaquinasVirtuais/MaquinasVirtuaisFun.jsx
+++ src/pages/ListagemServicos/MaquinasVirtuais/MaquinasVirtuaisFun.jsx
@@ -10,7 +10,7 @@ export default function MaquinasVirtuais() {
     const [dataCadastro, setDataCadastro] = useState("");
 
     function buscarMaquinas() {
-        axios('https://door-back.azurewebsites.net/api/MaquinaVirtuals', {
+        axios('https://back-door.azurewebsites.net/api/MaquinaVirtuals', {
             headers: {
                 'Authorization': 'Bearer ' + localStorage.getItem('usuario-login')
             }
